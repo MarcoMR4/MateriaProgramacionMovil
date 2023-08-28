81bac50e5cd90cdd2270d1e50fb32b4faf48883a
Marko <19121052@morelia.tecnm.mx>
2023-08-28 08:59:25 -0600

    Estructura de Login

--- Practica/App.js
+++ Practica/App.js
@@ -1,37 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, TouchableOpacity, View, Image, TextInput } from 'react-native';
 
 import CustomText  from './src/components/StyleText';
 import CustomTouchable from './src/components/StyleButton';
 
 export default function App() {
-
   
-  const press1 = () => {
-    console.log("Mensaje al presionar");
-  };
+  // console.log(heigh)
+  // const {width, heigh} = Dimensions.get("screen")
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Bienvenido a react native!</Text>
-
-      <CustomText fontSize={10} color="red" fontWeight="bold">
-        Texto custom
-      </CustomText>
-
-      <TouchableOpacity style={styles.buttom} onPress={() => console.log('Press')}>
-        <Text>Buttom</Text>
-      </TouchableOpacity>
       
-
-      <CustomTouchable
-        activeOpacity={0.9}
-        text="Custom button"
-        style = {{backgroundColor: "cyan"}}
-        onPress={press1}
-      />
-        
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -39,15 +19,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'silver',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
   text:{
-    color: 'blue',
+    color: 'black',
+    fontWeight: "bold",
   },
   buttom:{
     borderWidth: 5,
     padding: 5,
+  },
+  
+  input:{
+    borderWidth: 1,
+    width: 200,
+    borderRadius: 10,
+    height: 50
   }
 });
