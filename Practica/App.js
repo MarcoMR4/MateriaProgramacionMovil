17f48c905bc17bcc28999d9b6cc69485e979a2c4
Marko <19121052@morelia.tecnm.mx>
2023-08-31 09:23:40 -0600

    FlatList

--- Practica/App.js
+++ Practica/App.js
@@ -1,21 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { Dimensions, StyleSheet, Text, TouchableOpacity, View, Image, TextInput } from 'react-native';
+import { Dimensions, StyleSheet, FlatList, Text, TouchableOpacity, View, Image, TextInput } from 'react-native';
 
 import CustomText  from './src/components/StyleText';
 import CustomTouchable from './src/components/StyleButton';
 import Login from './src/screens';
+import { THEME } from './src/theme/colors';
 
 export default function App() {
   
   // console.log(heigh)
   // const {width, heigh} = Dimensions.get("screen")
+  const verde = THEME.COLORS.GREEN
+
+  const DATA = [
+    {id: 0, name: 'Marco1' ,apellido: 'Ramirez'},
+    {id: 1, name: 'Marco2' ,apellido: 'Ramirez'},
+    {id: 2, name: 'Marco3' ,apellido: 'Ramirez'},
+    {id: 3, name: 'Marco4' ,apellido: 'Ramirez'},
+    {id: 4, name: 'Marco5s' ,apellido: 'Ramirez'},
+  ]
 
   return (
-    <View style = {styles.container}>
-       <View style={styles.item}>
-        <Login></Login>
-       </View>  
-    </View>
+      <View style={styles.item}>
+        <FlatList
+          data ={ DATA}
+          renderItem = {( {item : {name, apellido}}) =>
+            <View style = {styles.elemento}>
+              <Text> {name} </Text>
+              <Text style = {{color: verde}}> {apellido} </Text>
+            </View>
+          }
+          ItemSeparatorComponent={
+            <Text>Separador</Text>
+          }
+        />
+      </View>  
   );
 }
 
@@ -26,9 +45,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   item: {
-    width: '100%',
+    flex: 1,
+    width: 400,
     height: '100%',
+    marginTop: 40,
+   
   },
+  elemento: {
+    borderWidth: 1,
+    justifyContent: 'center',
+  }
 });
 
 
