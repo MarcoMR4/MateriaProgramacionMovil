2369e2a51886a2ecd3463098985e5ff35b796bba
Marko <19121052@morelia.tecnm.mx>
2023-09-09 23:31:40 -0600

    last details

--- AwesomeProject/src/constants/symptoms.js
+++ AwesomeProject/src/constants/symptoms.js
@@ -1,8 +1,8 @@
 
 export const SYMPTOMS = [
     {id: 1, text: "I'm fine" ,emoji: '😅' },
-    {id: 1, text: "Depressed" ,emoji: '😥' },
-    {id: 1, text: "Angry" ,emoji: '😡' },
-    {id: 1, text: "Confused" ,emoji: '😵' },
-    {id: 1, text: "apathetic" ,emoji: '🙄' },
+    {id: 2, text: "Depressed" ,emoji: '😥' },
+    {id: 3, text: "Angry" ,emoji: '😡' },
+    {id: 4, text: "Confused" ,emoji: '😵' },
+    {id: 5, text: "apathetic" ,emoji: '🙄' },
 ]
