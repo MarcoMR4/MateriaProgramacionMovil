17ea8dc8c8bced253daeca882568198a03fb135a
Marko <19121052@morelia.tecnm.mx>
2023-09-11 08:58:28 -0600

    states

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import {
   StyleSheet,
@@ -5,19 +6,69 @@ import {
   View,
   StatusBar as ReactStatus,
   Platform,
+  TouchableOpacity,
+  TextInput,
 } from "react-native";
 import Constants from "expo-constants";
-import WelcomeScreen from "./src/screens/WelcomeScreen";
-import Welcome from "./src/screens/Task9/Welcome";
 import WelcomeBack from "./src/screens/Task9/WelcomeBack";
-import NewAccount from "./src/screens/Task9/NewAccount";
+
+
+//Hooks
+//Custom hooks, hooks personalizables
 
 export default function App() {
+
+  /*
+  const [state, setState] = useState({
+    nombre: 'Marco', 
+    edad: 23,
+    ciudad: 'Morelia', 
+    chido: true,
+  })
+  */
+
+  const [state, setState] = useState(1)
+
+  const [inputValue, setInputValue] = useState('Default value')
+
+  console.log(React.useState(), 'With React')
+  console.log(React)
+
+  //  console.log(state[0],'**STATE 0**')
+  //  console.log(state[1],'**STATE 1**')
+
+  
+ 
+  const sumValue = (value1) => setState(state + value1)
+  
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Welcome />
+
+      {/*<WelcomeBack />*/}
+      <Text>{JSON.stringify(state)}</Text>
+
+
+      <TouchableOpacity onPress={() => sumValue(10)}>
+        <Text>Change value</Text>
+      </TouchableOpacity>
+
+
+    
+      <TextInput
+        value={inputValue}
+        style = {{borderWidth:1, borderColor: 'black'}}
+        onChangeText={(value => setInputValue(value))}
+
+
+      />
+
+
+      <Text>
+        {inputValue}
+      </Text>
+
     </View>
   );
 }
@@ -27,6 +78,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "white",
     paddingTop: Constants.statusBarHeight,
-
   },
 });
---
+++ AwesomeProject/src/screens/practica10/NewScreen.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity, TextInput } from 'react-native';
+
+
+function NewAccount() {
+  return (
+    <View style={styles.container}>  
+        
+
+
+    </View>
+  );
+}
+
+export default NewAccount;
+
+const styles = StyleSheet.create({
+    
+    container:{
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        padding: 40,
+    },
+    
+
+})
