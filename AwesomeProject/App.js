ca9a698f0b5ad22ca83eeb3b679e35129a9cd365
Marko <19121052@morelia.tecnm.mx>
2023-10-31 09:01:51 -0600

    corregir Multiply counter

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,10 +1,13 @@
 import { LayoutCalculator } from './src/Layout/LayoutCalculator';
 import { View, StyleSheet } from 'react-native';
+import { LayoutMemo } from './src/Layout/Memo';
+
+//Practica 4: hook usememo 
 
 export default function App() {
 
   return (
-    <LayoutCalculator />
+    <LayoutMemo />
   );
 }
 
---
+++ AwesomeProject/src/Layout/Memo.jsx
@@ -0,0 +1,75 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Constants from "expo-constants";
+import { StatusBar } from 'expo-status-bar';
+import { useMemo, useState } from 'react';
+
+  
+
+
+export const LayoutMemo = () => {
+    // useCallBack to memorize a function 
+    // memo to memorize a component 
+    //Memorize a value
+    const multiplyCounter = useMemo(() => { //This is a value, not a function
+        console.log(counter)
+        return counter * 100
+
+    }, [counter])
+
+    const [counter, setCounter] = useState(0)
+    const [newCounter, setNewCounter] = useState(0)
+
+    // const multiplyCounter = () => {
+       //is a function 
+    // }
+
+    const sayHello = () => {
+        console.log("Say Hello!")
+    } 
+
+
+
+    return (
+        <View style={styles.container}>
+            
+            <TouchableOpacity 
+                onPress={() => multiplyCounter()}
+                style={{borderWidth: 0, padding: 10, margin: 5, borderRadius: 10}}>
+                <Text style={{fontSize: 25, fontWeight:'bold'}}>
+                    Multiply Counter: {multiplyCounter}
+                </Text>
+            </TouchableOpacity>
+
+            {/* <Text style={{fontSize: 30, fontWeight:'bold'}}>{counter}</Text> */}
+
+            <TouchableOpacity
+                onPress={() => {setCounter(counter+1)}}
+                style={{borderWidth: 1, padding: 10, margin: 5, borderRadius: 10}}>
+                <Text>Increment by one</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                onPress={() => {setNewCounter(counter+1)}}
+                style={{borderWidth: 1, padding: 10, margin: 5, borderRadius: 10}}>
+                <Text>Increment new counter</Text>
+            </TouchableOpacity>
+
+        <StatusBar style="auto" />
+    </View>
+    
+
+
+    )
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+      paddingTop: Constants.statusBarHeight,
+    },
+    
+});
