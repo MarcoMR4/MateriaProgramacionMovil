ae3469a1022faf5fa81f466c7b172631150d5db0
Marko <19121052@morelia.tecnm.mx>
2023-11-15 18:27:20 -0600

    practica 5

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,14 +1,27 @@
-import { LayoutCalculator } from './src/Layout/LayoutCalculator';
-import { View, StyleSheet } from 'react-native';
-import { LayoutMemo } from './src/Layout/Memo';
-
-//Practica 4: hook usememo 
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View} from "react-native";
+import Constants from "expo-constants";
+import { Provider, useSelector } from "react-redux";
+import { store } from "./src/store";
+import Counter from "./src/features/Counter";
 
 export default function App() {
-
   return (
-    <LayoutMemo />
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Counter />
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: Constants.statusBarHeight,
+  },
+});
